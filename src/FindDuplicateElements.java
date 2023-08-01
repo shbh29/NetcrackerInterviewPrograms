24bbf53f43a715e87000f00b0eb8a476d0b5c58b
Shubham Singh <ssingh@redseal.net>
2023-08-02 00:13:20 +0530

    using pure streams, this seems more time consuming and complex un necessary

--- src/FindDuplicateElements.java
+++ src/FindDuplicateElements.java
@@ -1,6 +1,12 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class FindDuplicateElements {
@@ -8,10 +14,18 @@ public class FindDuplicateElements {
         /**
          * How to find duplicate elements in a given integers list in java using Stream functions?
          */
-        List lst = List.of(15,20,10,25,10,25,7,8,2,15);
-        Set set = new HashSet();
-        lst.stream()
-                .filter(e -> !set.add(e))
-                .forEach(System.out::println);
+        List<Integer> lst = new ArrayList(Arrays.asList(1, 2, 3, 4, 5, 2, 4));
+        Set<Integer> repeatingNumbersSet = lst.stream()
+                .collect(Collectors.groupingBy(Function.identity(), LinkedHashMap::new, Collectors.counting()))
+                .entrySet()
+                .stream()
+                .filter(e -> e.getValue() > 1)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toSet());
+
+        System.out.println(repeatingNumbersSet);
+
+
+//                .forEach(System.out::println);
     }
 }
